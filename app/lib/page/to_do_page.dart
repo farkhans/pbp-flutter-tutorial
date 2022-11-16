9231e8dc66f574173f38ed3542d9e82172dac0ec
farkhans <farkhansyawalharahap@gmail.com>
2022-11-17 04:32:35 +0700

    Tutorial 8

--- app/lib/page/to_do_page.dart
+++ app/lib/page/to_do_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:app/model/to_do.dart';
 import 'form.dart';
 import '../main.dart';
+import '../model/future/to_do_future.dart';
 
 class ToDoPage extends StatefulWidget {
   const ToDoPage({super.key});
@@ -15,30 +13,6 @@ class ToDoPage extends StatefulWidget {
 }
 
 class _ToDoPageState extends State<ToDoPage> {
-  Future<List<ToDo>> fetchToDo() async {
-    var url = Uri.parse(
-        'https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10');
-    var response = await http.get(
-      url,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-    );
-
-    // melakukan decode response menjadi bentuk json
-    var data = jsonDecode(utf8.decode(response.bodyBytes));
-
-    // melakukan konversi data json menjadi object ToDo
-    List<ToDo> listToDo = [];
-    for (var d in data) {
-      if (d != null) {
-        listToDo.add(ToDo.fromJson(d));
-      }
-    }
-
-    return listToDo;
-  }
 
   @override
   Widget build(BuildContext context) {
